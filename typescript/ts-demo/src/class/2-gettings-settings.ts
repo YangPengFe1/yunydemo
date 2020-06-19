eccbc7af25dcda286305e65ff94a9063a88bfb95
YangPengFe1 <407575456@qq.com>
2020-06-19 11:07:08 +0800

    feat: add gettings settings

--- typescript/ts-demo/src/class/2-gettings-settings.ts
+++ typescript/ts-demo/src/class/2-gettings-settings.ts
@@ -2,26 +2,35 @@
  * @Author       : yangpf-c
  * @Date         : 2020-06-18 15:22:08
  * @LastEditors  : yangpf-c
- * @LastEditTime : 2020-06-18 15:45:04
+ * @LastEditTime : 2020-06-19 11:05:28
  * @FilePath     : \ts-demo\src\class\2-gettings-settings.ts
  * @Description  : 类-存取器
  */
 class Person2 {
   nameUser: string = 'tom'
-  private age: number = 18
-  getAge(): number {
-    return this.age
+  private _age: number = 18
+  // getAge(): number {
+  //   return this.age
+  // }
+  // setAge(age: number): void {
+  //   if (age > 0 && age < 200) {
+  //     this.age = age
+  //   }
+  // }
+  get aname(): number {
+    return this._age
   }
-  setAge(age: number): void {
-    if (age > 0 && age < 200) {
-      this.age = age
+  
+  set age(age: number) {
+    if (age > 0 && age < 150) {
+      this._age = age
     }
   }
 }
 
 let toms1: Person2 = new Person2()
-toms1.setAge(20)
-toms1.setAge(201)
-console.log(toms1.getAge())
+// toms1.setAge(20)
+// toms1.setAge(201)
+// console.log(toms1.getAge())
 
 // ! console.log(toms1.age)
